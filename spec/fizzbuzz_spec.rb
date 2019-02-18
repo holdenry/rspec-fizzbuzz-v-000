defb902b8585d094db2352c0213cc30b7c133a05
Robbie  H <ecofemale@hotmail.com>
2019-02-18 00:09:29 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -20,7 +20,6 @@ describe "fizzbuzz" do
     fizz_15 = fizzbuzz(15)
     fizzbuzz(15) == "FizzBuzz"
 
-    
   end
   it 'returns nil when the number is not divisible by 3 or 5' do
     fizz_4 = fizzbuzz(4)
